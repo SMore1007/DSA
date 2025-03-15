98b4e408ae0307de904d0d57ae026aae9f5a633d
Sagar More <sagarmore1007@gmail.com>
2025-03-15 18:10:14 +0530

    Binary Search

--- 04_Binary_Search/07_countFreq_2.cpp
+++ 04_Binary_Search/07_countFreq_2.cpp
@@ -1,48 +1,37 @@
-/*
-Given a sorted integers array containing a duplicates,
-count occurrences of given numbers
-
-*/
-
 #include<iostream>
 #include<vector>
 using namespace std;
 
-typedef struct 
+int findFirstIndexOfK(vector<int> &Arr, int k)
 {
-    int firstel;
-    int lastele;
-}IndexType_t;
-
-IndexType_t coutFreq(vector<int> &Arr, int k)
-{
-    int result[2];
-    int start = 0, end = Arr.size() - 1;
-
-    int first = -1;
-    int last = -1;
+    int start = 0;
+    int end = Arr.size() - 1;
+    int result = -1; // Fix
 
     while (start <= end)
     {
         int mid = start + (end - start) / 2;
-
         if (Arr[mid] == k)
         {
-            first = mid;  // Store the index
-            end = mid - 1;  // search in left
-            // start = mid + 1; // Continue searching in the right half
+            result = mid;
+            end = mid - 1;  // Continue searching in the left half
         }
-        else if (Arr[mid] < k)
-        {
-            start = mid + 1;
-        }
-        else
+        else if (k < Arr[mid])
         {
             end = mid - 1;
         }
+        else
+        {
+            start = mid + 1;
+        }
     }
-    result[0] = first;
-    start = 0, end = Arr.size() - 1;
+    return result; // Return -1 if not found
+}
+
+int findLastIndexOfK(vector<int> &Arr, int k)
+{
+    int start = 0, end = Arr.size() - 1;
+    int result = -1;
 
     while (start <= end)
     {
@@ -50,32 +39,29 @@ IndexType_t coutFreq(vector<int> &Arr, int k)
 
         if (Arr[mid] == k)
         {
-            last = mid;  // Store the index
+            result = mid;   // Store the index
             start = mid + 1; // Continue searching in the right half
         }
-        else if (Arr[mid] < k)
-        {
-            start = mid + 1;
-        }
-        else
+        else if (k < Arr[mid])
         {
             end = mid - 1;
         }
+        else
+        {
+            start = mid + 1;
+        }
     }
-    result[1] = last;
-
-    return {result[0], result[1]};
+    return result; // Return -1 if not found
 }
 
 int main()
 {
-    int count = 0;
-    vector<int> arr = {1,2,3,4,4,4,4,4,4,4,4,5};
-    int ele;
-    cout << "Enter the element which you want to cout os occurrance: ";
-    cin >> ele;
-
-    IndexType_t Index = coutFreq(arr, ele);
-    cout << "Total Count of Element "<< ele << " is " << Index.lastele - Index.firstel << " "<<endl;
+    int k;
+    cout << "Enter the element: ";
+    cin >> k ;
+    vector<int> arr = {2,2,2,3,4,5,6,6,6,6,6,6,8,9,10};
+    int firstIndex = findFirstIndexOfK(arr, k);
+    int lastIndex = findLastIndexOfK(arr, k);
+    cout << "Total Occurrence of " << k << " is: "<<lastIndex - firstIndex << " times." << endl;
     return 0;
 }
